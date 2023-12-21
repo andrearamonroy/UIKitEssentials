82032a2846bc9453dc43df2fd96a94151301f934
Andrea Monroy <54419381+andrearamonroy@users.noreply.github.com>
2023-12-21 15:21:22 -0600

    first commit

---
+++ UIKitEssentials/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitEssentials
+//
+//  Created by Andrea on 12/19/23.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
